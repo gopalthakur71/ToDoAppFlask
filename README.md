548f2afd62e47804644e8271e3b2a1202dc62f7b
Gopal Thakur <gopalthakur71@gmail.com>
2023-12-22 16:21:33 +0530

    Flask file added and README.md modified.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ToDoAppFlask
+
+This a simple Flask app that takes input and prints on the screen. 
+To fire this app up. Run the following commands in the terminal
+
+python app.py
---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+todos = []  # Initialize an empty list to store tasks
+
+@app.route('/')
+def index():
+    counter = len(todos) + 1  # Start the counter for each rendering
+    return render_template('index.html', todos=todos, counter=counter)
+
+@app.route('/add', methods=['POST'])
+def add_todo():
+    todo = request.form['todo']
+    todos.append(todo)
+    return redirect('/')
+
+@app.route('/delete/<int:index>')
+def delete_todo(index):
+    del todos[index]
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
